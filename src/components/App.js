361f382a712303dff84743b31978848855a412cb
islomnumanov <islom4ik.numanov@gmail.com>
2021-09-24 09:07:19 +0900

    App and Search bar has been added

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,24 @@
 import React from "react";
 import SearchBar from "./SearchBar";
+import google from "../apis/google";
 
 class App extends React.Component {
+  state = { information: [] };
+
+  onTermSubmit = async (term) => {
+    const response = await google.get("/search", {
+      params: {
+        q: term,
+      },
+    });
+
+    this.setState({ information: response.data.items });
+  };
+
   render() {
     return (
       <div>
-        <SearchBar />
+        <SearchBar onTermSubmit={this.onTermSubmit} />
       </div>
     );
   }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,29 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+  state = { term: "" };
+
+  onFormSubmit = (event) => {
+    event.preventDefault();
+
+    this.props.onTermSubmit(this.state.term);
+  };
+
+  onInputChange = (event) => {
+    this.setState({ term: event.target.value });
+  };
+
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
-            <label>Search </label>
-            <input type="text"></input>
+            <label>Enter Page Name</label>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={this.onInputChange}
+            ></input>
           </div>
         </form>
       </div>
