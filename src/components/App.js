6d0ea9c006274def444e76d2df40487d125a7536
islomnumanov <islom4ik.numanov@gmail.com>
2021-09-26 13:20:54 +0900

    VideoDetails complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import SearchBar from "./SearchBar";
+import VideoDetails from "./VideoDetails";
 import google from "../apis/google";
 
 class App extends React.Component {
-  state = { information: [] };
+  state = { information: [], selectedVideo: null };
 
   onTermSubmit = async (term) => {
     const response = await google.get("/search", {
@@ -12,13 +13,25 @@ class App extends React.Component {
       },
     });
 
-    this.setState({ information: response.data.items });
+    this.setState({
+      information: response.data.items,
+      selectedVideo: response.data.items[0],
+    });
+    console.log(this.state.selectedVideo);
   };
 
   render() {
     return (
-      <div>
+      <div className="ui container">
         <SearchBar onTermSubmit={this.onTermSubmit} />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetails video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column"></div>
+          </div>
+        </div>
       </div>
     );
   }
